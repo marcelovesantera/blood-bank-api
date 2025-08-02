6ba9b51ce3b21448803460e93a690a0ad5184a63
Marcelo Vesantera <mrvesantera@hotmail.com>
2025-08-02 11:54:35 -0300

    feat: endpoints base created

--- BloodBankAPI/Controllers/DonorController.cs
+++ BloodBankAPI/Controllers/DonorController.cs
@@ -6,6 +6,34 @@ namespace BloodBankAPI.Controllers
     [ApiController]
     public class DonorController : ControllerBase
     {
-        
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return NoContent();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll(string search)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
     }
 }
